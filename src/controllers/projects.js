a8656870df643b27d873cfd633f212b406445892
lansovinc <lan.sovinc@gmail.com>
2021-10-05 21:44:37 +0200

    Added controllers for projects and tasks.

---
+++ src/controllers/projects.js
@@ -0,0 +1,195 @@
+const mongoose = require('mongoose');
+const Project = mongoose.model('Project');
+
+
+// Return project with requested id
+exports.returnByID = (req, res) => {
+    Project.findById(req.params.id)
+        .then(project => {
+            if (!project) {
+                return res.status(404).json({
+                    message: "No project with selected ID!"
+                });
+            }
+            res.status(200).json(project);
+        })
+        .catch(error => {
+            res.status(500).send({
+                message: error.message || "An error occurred while fetching project!"
+            });
+        });
+};
+//----------------------------------------------------------------
+
+
+// Return projects with requested parameters
+exports.return = (req, res) => {
+
+    // Create filter object
+    let filterObject = {}
+    // Add query parameters to filter object
+    req.query.active && (filterObject["active"] = req.query.active)
+    req.query.members && (filterObject["members"] = req.query.members)
+
+    // TODO: Make query more efficient (should have)
+
+    Project.find(filterObject, 'title description active members createdAt updatedAt')
+        .sort({updatedAt: -1, _id: -1})
+        .skip(16 * (req.query.page || 0))
+        .limit(16)
+        .then(projects => {
+            if (!projects) {
+                return res.status(404).json({
+                    message: "No project with selected parameters!"
+                });
+            }
+
+            res.status(200).json(projects);
+        })
+        .catch(error => {
+            res.status(500).send({
+                message: error.message || "An error occurred while fetching projects!"
+            });
+        });
+};
+//----------------------------------------------------------------------------------------------------------------------
+
+
+// Create new project
+exports.create = (req, res) => {
+
+    // Validate request
+    if (!req.body.title || !req.body.description || !req.body.active || !req.body.members) {
+        return res.status(400).json({message: "Required data must be present"});
+    }
+
+    // Check title
+    if (typeof (req.body.title) !== "string" || req.body.title.length > 64) {
+        return res.status(400).json({
+            message: "Invalid title."
+        });
+    }
+
+    // Check description
+    if (typeof (req.body.description) !== "string" || req.body.description.length > 1024) {
+        return res.status(400).json({
+            message: "Invalid description."
+        });
+    }
+
+    // Check status
+    if (typeof (req.body.active) !== "boolean") {
+        return res.status(400).json({
+            message: "Invalid project status."
+        });
+    }
+
+    // TODO: Check memebers
+
+    // Create new project
+    let newProject = new Project({
+        title: req.body.title,
+        description: req.body.description,
+        active : req.body.active,
+        members : req.body.members
+    });
+
+    // Save new Project in the database
+    newProject
+        .save(newProject)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(error => {
+            res.status(500).send({
+                message: error.message || "An error occurred while creating a new property!"
+            });
+        });
+};
+//----------------------------------------------------------------------------------------------------------------------
+
+// Delete project
+exports.delete = (req, res) => {
+    Project.findByIdAndDelete(req.params.id)
+        .then(async project => {
+            if (!project) {
+                return res.status(404).send({
+                    message: `No project with selected ID!`
+                });
+            }
+            // TODO: delete tasks
+            res.send({message: "Project deleted!"});
+
+        })
+        .catch(error => {
+            res.status(500).send({
+                message: error.message || "An error occurred while deleting project!"
+            });
+        });
+};
+//----------------------------------------------------------------------------------------------------------------------
+
+// Update project
+exports.update = (req, res) => {
+
+    // Validate request
+    if (!req.body.title || !req.body.description || !req.body.active || !req.body.members) {
+        return res.status(400).json({message: "Required data must be present"});
+    }
+
+    // Check title
+    if (typeof (req.body.title) !== "string" || req.body.title.length > 64) {
+        return res.status(400).json({
+            message: "Invalid title."
+        });
+    }
+
+    // Check description
+    if (typeof (req.body.description) !== "string" || req.body.description.length > 1024) {
+        return res.status(400).json({
+            message: "Invalid description."
+        });
+    }
+
+    // Check status
+    if (typeof (req.body.active) !== "boolean") {
+        return res.status(400).json({
+            message: "Invalid project status."
+        });
+    }
+
+    // TODO: Check memebers
+
+    Project.findById(req.params.id)
+        .then(project => {
+
+            if (!project) {
+                return res.status(404).json({
+                    message: "No project with selected ID!"
+                });
+            }
+
+            // Update existing project
+            project.title = req.body.title;
+            project.description = req.body.description;
+            project.active = req.body.active;
+            project.members = req.body.members;
+
+            // Save updated project data
+            project.save()
+                .then(() => {
+                    res.status(200).json({message: "Project updated"})
+                })
+                .catch(error => {
+                    res.status(500).send({
+                        message: error.message || "An error occurred while updating project!"
+                    });
+                });
+        })
+        .catch(error => {
+            res.status(500).send({
+                message: error.message || "An error occurred while updating project!"
+            });
+        });
+};
+//----------------------------------------------------------------------------------------------------------------------
---
+++ src/controllers/tasks.js
@@ -0,0 +1,276 @@
+const mongoose = require('mongoose');
+const Task = mongoose.model('Task');
+
+
+// Return task with requested id
+exports.returnByID = (req, res) => {
+    Task.findById(req.params.id)
+        .then(task => {
+            if (!task) {
+                return res.status(404).json({
+                    message: "No task with selected ID!"
+                });
+            }
+            res.status(200).json(task);
+        })
+        .catch(error => {
+            res.status(500).send({
+                message: error.message || "An error occurred while fetching task!"
+            });
+        });
+};
+//----------------------------------------------------------------
+
+
+// Return task with requested parameters
+exports.return = (req, res) => {
+
+    // Create filter object
+    let filterObject = {}
+    // Add query parameters to filter object
+    req.query.project && (filterObject["project"] = req.query.project)
+    req.query.contributor && (filterObject["contributor"] = req.query.contributor)
+    req.query.completed && (filterObject["completed"] = req.query.completed)
+    req.query.paid && (filterObject["paid"] = req.query.paid)
+
+    // TODO: Make query more efficient (should have)
+
+    Task.find(filterObject, 'title note hours hourlyRate project date contributor completed paid createdAt updatedAt')
+        .sort({date: -1, _id: -1})
+        .skip(16 * (req.query.page || 0))
+        .limit(16)
+        .then(tasks => {
+            if (!tasks) {
+                return res.status(404).json({
+                    message: "No task with selected parameters!"
+                });
+            }
+
+            res.status(200).json(tasks);
+        })
+        .catch(error => {
+            res.status(500).send({
+                message: error.message || "An error occurred while fetching tasks!"
+            });
+        });
+};
+//----------------------------------------------------------------------------------------------------------------------
+
+
+// Create new task
+exports.create = (req, res) => {
+
+    // Validate request
+    if (!req.body.title || !req.body.note || !req.body.hours || !req.body.hourlyRate || !req.body.project || !req.body.date || !req.body.contributor || !req.body.completed || !req.body.paid) {
+        return res.status(400).json({message: "Required data must be present"});
+    }
+
+    // Check title
+    if (typeof (req.body.title) !== "string" || req.body.title.length > 64) {
+        return res.status(400).json({
+            message: "Invalid title."
+        });
+    }
+
+    // Check note
+    if (typeof (req.body.note) !== "string" || req.body.note.length > 1024) {
+        return res.status(400).json({
+            message: "Invalid note."
+        });
+    }
+
+    // Check hours
+    if (typeof (req.body.hours) !== "number" || req.body.hours < 0.5 || req.body.hours > 1000000) {
+        return res.status(400).json({
+            message: "Invalid hours."
+        });
+    }
+
+    // Check hourlyRate
+    if (typeof (req.body.hourlyRate) !== "number" || req.body.hourlyRate < 0.01 || req.body.hourlyRate > 1000000) {
+        return res.status(400).json({
+            message: "Invalid hourlyRate."
+        });
+    }
+
+    // Check note
+    if (typeof (req.body.note) !== "string" || req.body.note.length > 1024) {
+        return res.status(400).json({
+            message: "Invalid note."
+        });
+    }
+
+    // Check date
+    if (req.body.date instanceof Date) {
+        return res.status(400).json({
+            message: "Invalid date."
+        });
+    }
+
+    // Check completed status
+    if (typeof (req.body.completed) !== "boolean") {
+        return res.status(400).json({
+            message: "Invalid completed status."
+        });
+    }
+
+    // Check paid status
+    if (typeof (req.body.paid) !== "boolean") {
+        return res.status(400).json({
+            message: "Invalid paid status."
+        });
+    }
+
+    // TODO: Check contributor & project
+
+    // Create new task
+    let newTask = new Task({
+        title: req.body.title,
+        note: req.body.note,
+        hours : req.body.hours,
+        hourlyRate : req.body.hourlyRate,
+        project : req.body.project,
+        date : req.body.date,
+        contributor : req.body.contributor,
+        completed : req.body.completed,
+        paid : req.body.paid
+    });
+
+    // Save new task in the database
+    newTask
+        .save(newTask)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(error => {
+            res.status(500).send({
+                message: error.message || "An error occurred while creating new task!"
+            });
+        });
+};
+//----------------------------------------------------------------------------------------------------------------------
+
+// Delete project
+exports.delete = (req, res) => {
+    Task.findByIdAndDelete(req.params.id)
+        .then(async task => {
+            if (!task) {
+                return res.status(404).send({
+                    message: `No task with selected ID!`
+                });
+            }
+            res.send({message: "Task deleted!"});
+
+        })
+        .catch(error => {
+            res.status(500).send({
+                message: error.message || "An error occurred while deleting task!"
+            });
+        });
+};
+//----------------------------------------------------------------------------------------------------------------------
+
+// Update task
+exports.update = (req, res) => {
+
+    // Validate request
+    if (!req.body.title || !req.body.note || !req.body.hours || !req.body.hourlyRate || !req.body.project || !req.body.date || !req.body.contributor || !req.body.completed || !req.body.paid) {
+        return res.status(400).json({message: "Required data must be present"});
+    }
+
+    // Check title
+    if (typeof (req.body.title) !== "string" || req.body.title.length > 64) {
+        return res.status(400).json({
+            message: "Invalid title."
+        });
+    }
+
+    // Check note
+    if (typeof (req.body.note) !== "string" || req.body.note.length > 1024) {
+        return res.status(400).json({
+            message: "Invalid note."
+        });
+    }
+
+    // Check hours
+    if (typeof (req.body.hours) !== "number" || req.body.hours < 0.5 || req.body.hours > 1000000) {
+        return res.status(400).json({
+            message: "Invalid hours."
+        });
+    }
+
+    // Check hourlyRate
+    if (typeof (req.body.hourlyRate) !== "number" || req.body.hourlyRate < 0.01 || req.body.hourlyRate > 1000000) {
+        return res.status(400).json({
+            message: "Invalid hourlyRate."
+        });
+    }
+
+    // Check note
+    if (typeof (req.body.note) !== "string" || req.body.note.length > 1024) {
+        return res.status(400).json({
+            message: "Invalid note."
+        });
+    }
+
+    // Check date
+    if (req.body.date instanceof Date) {
+        return res.status(400).json({
+            message: "Invalid date."
+        });
+    }
+
+    // Check completed status
+    if (typeof (req.body.completed) !== "boolean") {
+        return res.status(400).json({
+            message: "Invalid completed status."
+        });
+    }
+
+    // Check paid status
+    if (typeof (req.body.paid) !== "boolean") {
+        return res.status(400).json({
+            message: "Invalid paid status."
+        });
+    }
+
+    // TODO: Check contributor & project
+
+    Task.findById(req.params.id)
+        .then(task => {
+
+            if (!task) {
+                return res.status(404).json({
+                    message: "No task with selected ID!"
+                });
+            }
+
+            // Update existing task
+            task.title = req.body.title;
+            task.note = req.body.note;
+            task.hours = req.body.hours;
+            task.hourlyRate = req.body.hourlyRate;
+            task.project = req.body.project;
+            task.date = req.body.date;
+            task.contributor = req.body.contributor;
+            task.completed = req.body.completed;
+            task.paid = req.body.paid;
+
+            // Save updated task data
+            task.save()
+                .then(() => {
+                    res.status(200).json({message: "Task updated"})
+                })
+                .catch(error => {
+                    res.status(500).send({
+                        message: error.message || "An error occurred while updating task!"
+                    });
+                });
+        })
+        .catch(error => {
+            res.status(500).send({
+                message: error.message || "An error occurred while updating task!"
+            });
+        });
+};
+//----------------------------------------------------------------------------------------------------------------------
