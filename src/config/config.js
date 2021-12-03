84fddfd6873dae1c92d645086de47252950a46a3
lansovinc <lan.sovinc@gmail.com>
2021-12-03 19:28:27 +0100

    Change port to 3000.

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    port: process.env.PORT || 5000,
+    port: process.env.PORT || 3000,
     url: process.env.URL || "http://localhost",
     dbURI: process.env.MONGODB_CLOUD_URI || 'mongodb://localhost/ManageBlue',
     secret: process.env.JWT_SECRET || "local development secret"
