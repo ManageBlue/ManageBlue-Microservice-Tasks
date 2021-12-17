1c192ed0bc5668921fa9136b10fb65393fceb753
Matej Horvat <matej@horvat.si>
2021-12-17 19:26:49 +0100

    Add logging to console

---
+++ src/middlewares/morgan_logger.js
@@ -0,0 +1,28 @@
+// morgan logging
+
+const morgan = require('morgan')
+const logger = require('./winston_logger')
+
+logger.stream = {
+    write: function(message, encoding){
+
+        // parse status out of morgan message
+        let status = (message.split('<')[1].split('>')[0])
+
+        // log with winston depending on response status
+        if(status >= 300){
+            logger.warn(message)
+        }
+        else if(status >= 400){
+            logger.error(message)
+        }
+        else{
+            logger.info(message);
+        }
+
+    }
+};
+
+// set morgan logging for express, then export it
+const morgan_log = morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" <:status> :res[content-length] ":referrer" ":user-agent"', { "stream": logger.stream })
+module.exports = morgan_log
